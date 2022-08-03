7b59efee7d521ff99b135cfa88d54080f1a725a3
Eman Mohammed <emohammdd073@gmail.com>
2022-08-03 11:45:58 +0200

    Update main.js
    adding clear form function

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 
 
 //getting all inputs from html to be used in js
-var logInBtn=document.getElementById('logInBtn');
-var signUpBtn=document.getElementById('signUpBtn');
-var loginBtnLink=document.getElementById('loginBtnLink')
+let logInBtn=document.getElementById('logInBtn');
+let signUpBtn=document.getElementById('signUpBtn');
+let loginBtnLink=document.getElementById('loginBtnLink')
 
-var logInEmail=document.getElementById('logInEmail')
-var logInPassword=document.getElementById('logInPassword');
-var testInputs=document.getElementById('testInputs');
-var userName=document.getElementById('userName');
-var userPassword=document.getElementById('userPassword');
-var userEmail=document.getElementById('userEmail');
-var checkInputs=document.getElementById('checkInputs');
+let logInEmail=document.getElementById('logInEmail')
+let logInPassword=document.getElementById('logInPassword');
+let testInputs=document.getElementById('testInputs');
+let userName=document.getElementById('userName');
+let userPassword=document.getElementById('userPassword');
+let userEmail=document.getElementById('userEmail');
+let checkInputs=document.getElementById('checkInputs');
 
-var userNameTesting=document.getElementById('userNameTesting');
+let userNameTesting=document.getElementById('userNameTesting');
 
 //the array in which dataof user will be stored
-var arrayOfSignUp;
-var userNameShow=localStorage.getItem('usernametest');
+let arrayOfSignUp;
+let userNameShow=localStorage.getItem('usernametest');
 //in order that the array contain old data
     if(localStorage.getItem('userinfo')!=null)
     {
@@ -31,8 +31,8 @@ var userNameShow=localStorage.getItem('usernametest');
 //check if the entered email ia already exit before
     function identicalEmail()
     {
-        var userEmailValue=userEmail.value;
-        for(var i=0;i<arrayOfSignUp.length;i++)
+        let userEmailValue=userEmail.value;
+        for(let i=0;i<arrayOfSignUp.length;i++)
         {
             if(arrayOfSignUp[i].email.toLowerCase() ==userEmailValue.toLowerCase())
             {
@@ -43,10 +43,23 @@ var userNameShow=localStorage.getItem('usernametest');
         }
     }
 
+
+function clearForm(){
+
+userName.value="";
+userEmail.value="";
+userPassword.value="";
+
+
+
+}
+
+
+
 //signing up
 function signup(){
     //store data of user as an object
-    var userData=
+    let userData=
     {
         name:userName.value,
         email:userEmail.value,
@@ -54,19 +67,13 @@ function signup(){
     }
     //getting the value on inputs
 
-        var userNameValue=userName.value;
-        var userEmailValue=userEmail.value;
-        var userPasswordValue=userPassword.value;
+        let userNameValue=userName.value;
+        let userEmailValue=userEmail.value;
+        let userPasswordValue=userPassword.value;
     
-    if(arrayOfSignUp.length==0)
-    {
-        arrayOfSignUp.push(userData);
-        localStorage.setItem('userinfo', JSON.stringify(arrayOfSignUp));
-        checkInputs.innerHTML = '<span class="text-success m-3">Success</span>';
-                    // return true;
-    }
+ 
  //check if any input is empty
-    if(userEmailValue=="" &&userPasswordValue=="" && userNameValue=="")
+    if(userEmailValue=="" ||userPasswordValue=="" ||userNameValue=="")
     {
         //return this message
         checkInputs.innerHTML= '<span class="text-danger m-3">All inputs is required</span>';
@@ -81,7 +88,7 @@ function signup(){
         arrayOfSignUp.push(userData);
         localStorage.setItem('userinfo', JSON.stringify(arrayOfSignUp));
      checkInputs.innerHTML = '<span class="text-success m-3">Success</span>'
-     console.log(arrayOfSignUp)
+     clearForm();
 
     }
 }
@@ -100,12 +107,12 @@ function signup(){
 function login()
 {
    
-    var logInEmailValue=logInEmail.value;
-    var logInPasswordValue=logInPassword.value;
+    let logInEmailValue=logInEmail.value;
+    let logInPasswordValue=logInPassword.value;
 
  //check if there is no inputs
 
-    if(logInEmailValue=="" && logInPasswordValue=="" )
+    if(logInEmailValue=="" || logInPasswordValue=="" )
     {
         //return this message
         testInputs.innerHTML= '<span class="text-danger m-3">All inputs is required</span>';
@@ -115,7 +122,7 @@ function login()
     else
     {
         //if sign in email &passowrd ara the same of array email & password
-        for(var i=0;i<arrayOfSignUp.length;i++)
+        for(let i=0;i<arrayOfSignUp.length;i++)
         {
             if(logInEmailValue==arrayOfSignUp[i].email && logInPasswordValue==arrayOfSignUp[i].password)
             {
